151b8a516f2dabe2732352f009f45c2e5902ae13
lucaguerrieri <luca.guerrieri@frb.gov>
2013-06-26 17:39:35 -0400

    Val generalized this file.

--- MATLAB/nim_lsc_model_2steps/utilities_2steps/load_data_ml.m
+++ MATLAB/nim_lsc_model_2steps/utilities_2steps/load_data_ml.m
@@ -15,12 +15,12 @@ yields_varlist = char('SVENY0025','SVENY0050','SVENY0075','SVENY0100','SVENY0200
 %Bring in Net Interest Margins from Call Report (TINY); calculated as nim_top25 =
 %100*netintinc/interest earning assets (where each component in the ratio
 %is calculated at each quarter based on the top 25 firms, by assets).
-nims = csvread('..\data\tiny_nims.csv', 1, 1);
-dates_nim = 1985.0:.25:2013.0;
-start_pos_nim = find(dates_nim==start_yobs);
-end_pos_nim = find(dates_nim==end_yobs);
-nims = 4*nims(start_pos_nim:end_pos_nim,1)'; %Multiply by 4 to annualize
-nims_varlist = char('nim_top25');
+%nims = csvread('..\data\tiny_nims.csv', 1, 1);
+%dates_nim = 1985.0:.25:2013.0;
+%start_pos_nim = find(dates_nim==start_yobs);
+%end_pos_nim = find(dates_nim==end_yobs);
+%nims = 4*nims(start_pos_nim:end_pos_nim,1)'; %Multiply by 4 to annualize
+%nims_varlist = char('nim_top25');
 
 %Bring in shadow banking share of financial sector assets. We use it as 
 %a measure of competitiveness in the banking sector.
@@ -36,6 +36,65 @@ end
 
 %This data has same dates as yields
 shadow_bank_share_assets = shadow_bank_share_assets_file(start_pos:end_pos,1)';
+
+%Bring in NIMs and subcategories of interest-earning assets. SEE pulltiny_nims.sas FILE TO SEE EXACTLY 
+%WHICH CALL REPORT SERIES WERE USED TO CONSTRUCT THESE DIFFERENT SERIES. 
+%Eight (8) variants of NIM are possible:
+% 1. With trading assets and based on end-of-period total assets rankings
+% 2. Without trading assets and based on end-of-period total assets rankings
+% 3. With trading assets and based on quarterly average of total assets
+% rankings (quarterly average constructed from daily data on assets)
+% 4. Without trading assets and based on quarterly average of total assets
+% rankings (quarterly average constructed from daily data on assets)
+% 5-8. Same as above, but based on interest-earning (ie) asset rankings (rather
+% than total asset rankings).
+
+if isunix
+nim_option = ['/ofs/work_mpa/m1vxb00/nim_project/MATLAB/data/data_ranked_by_total_assets/nims_assets_by_endperiod_total_assets.csv';
+               '/ofs/work_mpa/m1vxb00/nim_project/MATLAB/data/data_ranked_by_total_assets/nims_assets_by_quartavg_total_assets.csv';
+               '/ofs/work_mpa/m1vxb00/nim_project/MATLAB/data/data_ranked_by_ie_assets/nims_assets_by_endperiod_ie_assets.csv';
+               '/ofs/work_mpa/m1vxb00/nim_project/MATLAB/data/data_ranked_by_ie_assets/nims_assets_by_quartavg_ie_assets.csv'];
+else
+nim_option = ['..\data\nims_assets_by_endperiod_total_assets.csv';
+               '..\data\nims_assets_by_quartavg_total_assets.csv';
+               '..\data\nims_assets_by_endperiod_ie_assets.csv';
+               '..\data\nims_assets_by_quartavg_ie_assets.csv'];
+end           
+    
+%Use in the nim_option field to select the series
+%desired.
+by_end_period_total_assets = 1;
+by_quartavg_total_assets = 2;
+by_endperiod_ie_assets = 3;
+by_quartavg_ie_assets = 4;           
+           
+nim_with_trading = csvread(nim_option(by_endperiod_total_assets), 1, 1);           
+nim_no_trading = csvread(nim_option(by_endperiod_total_assets), 1, 2);
+
+%nim possibilities = [nim_with_trading_by_endperiod_total_assets;
+%                     nim_no_trading_by_endperiod_total_assets;
+%                     nim_with_trading_by_quartavg_total_assets;
+%                     nim_no_trading_by_quartavg_total_assets;
+%                     nim_with_trading_by_endperiod_ie_assets;
+%                     nim_no_trading_by_endperiod_ie_assets;
+%                     nim_with_trading_by_quartavg_ie_assets;
+%                     nim_no_trading_by_quartavg_ie_assets];
+
+dates_nim = 1985.0:.25:2013.0;
+start_pos_nim = find(dates_nim==start_yobs);
+end_pos_nim = find(dates_nim==end_yobs);
+nims = 4*nim_with_trading(start_pos_nim:end_pos_nim,1)'; %Multiply by 4 to annualize
+nims_varlist = char('nim_top25');
+
+%Bring in different asset series. All are in thousands of dollars and have same dates as NIMs above.%
+total_interest_earning_assets = csvread(nim_option(by_end_period_total_assets), 1, 3);
+assets_depository_inst = csvread(nim_option(by_end_period_total_assets), 1, 4);
+assets_securities_notrade = csvread(nim_option(by_end_period_total_assets), 1, 5);
+assets_fedfunds = csvread(nim_option(by_end_period_total_assets), 1, 6);
+assets_all_loans = csvread(nim_option(by_end_period_total_assets), 1, 7);
+assets_trading_accnts = csvread(nim_option(by_end_period_total_assets), 1, 8);
+
+
 
 yobs = yields; %before also had nims
 nfactors = 3;
