4bff5f50a2c007799e83c6f0cda77cf788a60d58
Luca Guerrieri <luca.guerrieri@frb.gov>
2013-07-14 22:38:21 -0400

    made paths compatible with linux

--- MATLAB/nim_lsc_model_2steps/setpath.m
+++ MATLAB/nim_lsc_model_2steps/setpath.m
@@ -6,6 +6,12 @@ path1 = '.\utilities_2steps';
 path2 = '..\estimation_utilities';
 path3 = '..\plot_support';
 
+if isunix
+    path1 = strrep(path1,'\','/');
+    path2 = strrep(path2,'\','/');
+    path3 = strrep(path3,'\','/');
+end
+
 
 path (path,path1);
 path (path,path2);
